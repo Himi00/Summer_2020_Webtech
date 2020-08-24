063e2fbad1ebef1dec53f318e05531f300a329d8
Humayra Himi <61056268+Himi00@users.noreply.github.com>
2020-08-24 20:55:31 +0600

    DOB validation

---
+++ FINAL_LAB_TASK_02/DATE OF BIRTH/dob.js
@@ -0,0 +1,30 @@
+function validateDOB()
+{
+	var day = document.forms["dobForm"]["dd"].value;
+	var month = document.forms["dobForm"]["mm"].value;
+	var year = document.forms["dobForm"]["yy"].value;
+	if ((date== "" ) || (month==" ") || (year==" ")) 
+	{
+		alert("Date Required");
+		return false;
+	}
+	else if((day<1) || (day>31))
+	{
+		alert("Day should be between 1-31");
+		return false;
+	}
+	else if((month<1) || (month>12))
+	{
+		alert("Month should be between 1-12");
+		return false;
+	}
+	else if((year<1900) || (year>2016))
+	{
+		alert("Year should be between 1900-2020");
+		return false;
+	}
+	else
+	{ 
+		return true;
+	}
+}
