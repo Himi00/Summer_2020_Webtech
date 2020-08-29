e37419588efc723d61f478c2da06c4be98dbe811
Humayra Himi <61056268+Himi00@users.noreply.github.com>
2020-08-29 20:18:33 +0600

    JS Validation

--- FINAL_LABTASK_3/person_profile.js
+++ FINAL_LABTASK_3/person_profile.js
@@ -7,56 +7,57 @@ function validateName()
            if (name == "")
              {  
             document.getElementById("nameErr").innerHTML="field can't be empty";
-                return false;
+               // return false;
              }
-          else if((name.charAt(0) <= 'A' || name.charAt(0)>='Z') && (name.charAt(0) <='a'|| name.charAt(0)>='z'))
+           if((name.charAt(0) <= 'A' || name.charAt(0)>='Z') && (name.charAt(0) <='a'|| name.charAt(0)>='z'))
 	        {
             document.getElementById("nameErr").innerHTML="Name should have alphabests only";
-		         return false;
+		         //return false;
 	        }
-	      else if(name.split("").length =< 2)
+	       if(name.split("").length <= 2)
 	        {
             document.getElementById("nameErr").innerHTML="Name can not have only 2 letters in it ";
-		         return false;
+		         //return false;
 	        }
-	      else
-	        { 
-		        return true;
-	        }
+	      if(name.split("").length>2)
+	      {
+	      	document.getElementById("nameErr").innerHTML="";
+
+	      }
 
     }
 function validateEmail()
     {
-	     var name = document.getElementById("email").value;
+	     var email = document.getElementById("email").value;
 	     if (email=="") 
 	        {
-            document.getElementById("emailErr").innerHTML="field can't be empty";
-		        return false;
-	        }
-	     if ((email.indexOf("@")=<0) || (email.indexOf(".com")=<0)) 
+	        	console.log("field can't be empty");
+            document.getElementById("emailErr").innerHTML="field can not be empty";
+		    }
+
+	     if ((email.indexOf("@")<=0) || (email.indexOf(".com")<=0)) 
 	        {
+	        	console.log("Invalid email Example: something@some.com");
             document.getElementById("emailErr").innerHTML="Invalid email Example: something@some.com";
-		        return false;
-	        }
-	     else
-	        { 
-		        return true;
-	        }
+		    }
+        if ((email.indexOf("@")>0) || (email.indexOf(".com")>0)) 
+	        {
+            document.getElementById("emailErr").innerHTML="";
+		    }	     
 
     }
 function validateGender()
     {
-	      var name = document.getElementById("gender").value;
-	      if (gender==" ") 
+	      var gender = document.getElementById("gender").value;
+	      if (gender=="") 
 	         {
             document.getElementById("genderErr").innerHTML="you must select gender";
-		         return false;
 	         }
-	      else
-	         { 
-		         return true;
-	         }
-
+	      if (gender==" ") 
+	         {
+            document.getElementById("genderErr").innerHTML="";
+	         }	         
+	      
      }
 
 function validateDOB()
@@ -66,19 +67,15 @@ function validateDOB()
 	if (date=="") 
 	{
         document.getElementById("dobErr").innerHTML="you must select Date of Birth";
-		return false;
 	}
 	
-	else
-	{ 
-		return true;
-	}
+	
 
     }
 function validateBG()
 {
    var bg = document.getElementById("bg").value;	
-	if(	var date = document.getElementById("dob").selectedIndex == 0;
+	if(	 bg = document.getElementById("bg").selectedIndex == 0
 ) 
 	{
         document.getElementById("bgErr").innerHTML="you must select Blood Group";
@@ -94,12 +91,8 @@ function validateDegree()
 	if (degree=="") 
 	{
 		alert("At least one of them has to be selected");
-		return false;
 	}
-	else
-	{ 
-		return true;
-	}
+	
 }
 
 function validateProfilepic()
@@ -109,20 +102,29 @@ function validateProfilepic()
 
 	 if(photo== "")
 	{
-		alert("Picture Required");
+        document.getElementById("photoErr").innerHTML="you must select photo";
 	}
-	else
-	{ 
-		return true;
-	}
+	
 }
 
 function validation()
 {
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+	  var gender = document.getElementById("gender").value;
+	  var date = document.getElementById("dob").value;
+	  var bg = document.getElementById("bg").value;
+	  var degree = document.getElementById("degree").value;
+      var photo = document.getElementById("photo").value;	
+	
+	
 
-if( validateName()==empty || validateEmail()== empty || validateGender()== empty || validateDOB()==empty 
-    || validateBG()==empty || validateDegree()==empty || validateProfilepic()==empty)
+
+
+
+if( name==""|| email=="" || gender=="" || date=="" || bg=="" || degree=="" || photo=="")
     {
+
      alert("please Fill up the form ");
     }
     else 
